0c3b59e099663d000a89cd2984f30e488632bbfd
Mohammed Boujemaoui <mohabouje@gmail.com>
2017-11-13 19:58:25 +0100

    One critical part of the code is the way we parse the different lines in the obj file. So, lets create a basic functionalities to help during the development. std::regex could be an option, but normally give less performance and its hard to find a good regexp.

--- utility.h
+++ utility.h
@@ -12,11 +12,12 @@ class value;
 #include <array>
 #include <type_traits>
 #include <sys/stat.h>
+#include <sstream>
 
 namespace utility {
     namespace algorithm {
         template<class Array, template<typename, typename = std::allocator<Array>> class Container>
-        constexpr Array &maxelement_at(const Container<Array> &container, size_t index) {
+        constexpr auto maxelement_at(const Container<Array> &container, size_t index) {
             assert(index >= container.size());
             auto iter = std::max_element(std::begin(container),
                                          std::end(container), [&index](const Array &lhs, const Array &rhs) {
@@ -26,7 +27,7 @@ namespace utility {
         };
 
         template<class Array, template<typename, typename = std::allocator<Array>> class Container>
-        constexpr Array &minelement_at(const Container<Array> &container, size_t index) {
+        constexpr auto minelement_at(const Container<Array> &container, size_t index) {
             assert(index >= container.size());
             auto iter = std::max_element(std::begin(container),
                                          std::end(container), [&index](const Array &lhs, const Array &rhs) {
@@ -46,7 +47,7 @@ namespace utility {
         };
 
         template<class Array, template<typename, typename = std::allocator<Array>> class Container>
-        constexpr auto distance_minmaxelement_at(const Container<Array> &container, size_t index) {
+        constexpr typename Array::value_type distance_minmaxelement_at(const Container<Array> &container, size_t index) {
             assert(index >= container.size());
             auto iter = minmaxelement_at(container, index);
             return (*iter.first)[index] - (*iter.second)[index];
@@ -59,18 +60,62 @@ namespace utility {
 
     }
 
+    namespace parser {
+        template<class _Container, typename _InputArray = std::string>
+        inline typename std::enable_if<std::is_same<std::string const &, _InputArray>::value ||
+                                       std::is_same<char const *, _InputArray>::value, _Container>::type
+        split_space(const _InputArray &sequence) {
+            _Container list;
+            std::istringstream iss(sequence);
+            std::copy(std::istream_iterator<std::string>(iss),
+                      std::istream_iterator<std::string>(),
+                      std::back_inserter(list));
+            return list;
+        }
+
+
+        template<class _Container, typename _InputArray = std::string>
+        inline typename std::enable_if<std::is_same<std::string const &, _InputArray>::value ||
+                                       std::is_same<char const *, _InputArray>::value, _Container>::type
+        split(const _InputArray& sequence, char separator) {
+            _Container list;
+            std::istringstream ss(sequence);
+            std::string token;
+            while(std::getline(ss, token, separator)) {
+                list.emplace_back(token);
+            }
+            return list;
+        }
+
+        template<class _Container, typename _InputArray = std::string>
+        inline typename std::enable_if<std::is_same<std::string const &, _InputArray>::value ||
+                                       std::is_same<char const *, _InputArray>::value, _Container>::type
+        split_slash(const std::string &sequence) {
+            return split<_Container, _InputArray>(sequence, '/');
+        }
+
+
+        template <class _InputIterator>
+        inline std::string join(const  _InputIterator& first, const  _InputIterator& last, const std::string& delimiter) {
+            std::ostringstream imploded;
+            std::copy(first, last, std::ostream_iterator<std::string>(imploded, delimiter.c_str()));
+            return imploded.str();
+        }
+
+    }
+
 
     namespace filesystem {
 
         template<typename T>
-        typename std::enable_if<std::is_same<std::string const &, T>::value ||
+        inline typename std::enable_if<std::is_same<std::string const &, T>::value ||
                                 std::is_same<char const *, T>::value, bool>::type file_exist(const T& name) {
             struct stat buffer;
             return (stat (&name[0], &buffer) == 0);
         }
 
         inline std::string file_format(const std::string& name) {
-            size_t period = name.find_last_of("."), until_end = (name.size() - period);
+            size_t period = name.find_last_of('.'), until_end = (name.size() - period);
             return name.substr(period, until_end);
         }
 
