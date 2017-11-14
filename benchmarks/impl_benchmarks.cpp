19a2e1a44c21bbe82f05444250326be2a128a2af
Mohammed Boujemaoui <mohabouje@gmail.com>
2017-11-14 17:49:45 +0100

    Implementing benchmark for the join function.
    Best copy & paste ever! This deserve an Official Sound Track: https://www.youtube.com/watch?v=oavMtUWDBTM

--- benchmarks/impl_benchmarks.cpp
+++ benchmarks/impl_benchmarks.cpp
@@ -37,10 +37,23 @@ static void BM_Split_Slash(benchmark::State &state) {
         parser::split_slash<Container, std::string>(tmp);
 }
 
-
+template <typename Container>
+void BM_Join_List_String(benchmark::State &state) {
+    const Container tmp = {"Never gonna give you up\n",
+                                          "Never gonna let you down\n",
+                                          "Never gonna run around and desert you\n",
+                                          "Never gonna make you cry\n",
+                                          "Never gonna say goodbye\n",
+                                          "Never gonna tell a lie and hurt you"};
+    for (auto _ : state)
+        parser::join(std::begin(tmp), std::end(tmp), " ");
+}
 
 BENCHMARK_TEMPLATE(BM_Split_Spaces, std::vector<std::string>);
 BENCHMARK_TEMPLATE(BM_Split_Spaces, std::list<std::string>);
 BENCHMARK_TEMPLATE(BM_Split_Slash, std::vector<std::string>);
 BENCHMARK_TEMPLATE(BM_Split_Slash, std::list<std::string>);
+
+BENCHMARK_TEMPLATE(BM_Join_List_String, std::vector<std::string>);
+BENCHMARK_TEMPLATE(BM_Join_List_String, std::list<std::string>);
 
