d1676a67e98ee4f477ae6c30cbafddb5745c250f
Mohammed Boujemaoui <mohabouje@gmail.com>
2017-11-14 22:13:52 +0100

    Find a problem during the testing with the v parameter parsing.
    Here we go! After 8 hours of work, 2 hours of french classes, now its time to finish this!

--- test/objformat_test.cpp
+++ test/objformat_test.cpp
@@ -3,14 +3,19 @@
 //
 
 #include <fstream>
+#include <random>
 #include "../Catch2/include/catch.hpp"
 #include "../objformat_reader.h"
 
+using namespace std;
+using namespace chrono;
 SCENARIO("Testing object format parser") {
+    const string my_file = "test.thing";
+    const string my_obj = "test.obj";
     ObjFormatReader reader;
+
     GIVEN("A file format archive") {
-        const std::string my_file = "test.thing";
-        const std::string my_obj = "test.obj";
+
         WHEN("The file doesnt exist") {
             remove(my_file.c_str());
             AND_WHEN("We try to parse the archive") {
@@ -23,7 +28,7 @@ SCENARIO("Testing object format parser") {
 
         WHEN( "The file is empty & not .obj format" ) {
             remove(my_file.c_str());
-            std::ofstream outfile (my_file.c_str());
+            ofstream outfile (my_file.c_str());
             AND_WHEN("We try to parse the archive that is not an .obj format") {
                 auto error = reader.load_file(my_file);
                 THEN("The error should be: FormatNotSupported"){
@@ -34,7 +39,7 @@ SCENARIO("Testing object format parser") {
 
         WHEN( "The file is empty and & .obj format" ) {
             remove(my_obj.c_str());
-            std::ofstream outfile (my_obj.c_str());
+            ofstream outfile (my_obj.c_str());
             AND_WHEN("We try to parse the archive that is an .obj format") {
                 auto error = reader.load_file(my_obj);
                 THEN("The error should be: File empty"){
@@ -43,5 +48,48 @@ SCENARIO("Testing object format parser") {
             }
         }
     }
+
+    remove(my_obj.c_str());
+    remove(my_file.c_str());
+
+}
+
+SCENARIO("Loading a basic .obj file") {
+    const string my_obj = "test.obj";
+    ofstream outfile(my_obj);
+    srand(static_cast<unsigned int>(duration_cast< milliseconds >(system_clock::now().time_since_epoch()).count()));
+    default_random_engine generator;
+    uniform_real_distribution<ObjPosition::value_type > distribution(0.0, 1.0);
+
+
+    GIVEN("A vertex obj that we will save in the file X times") {
+        const size_t num = 1000;
+        const ObjPosition v = { distribution(generator), distribution(generator), distribution(generator), distribution(generator)};
+        for (size_t i = 0; i < num; ++i) {
+            outfile << "v " << v[0] <<  " " << v[1] << " " << v[2]  << " " << v[3] << endl;
+        }
+
+        WHEN("We read the file from the parser") {
+            ObjFormatReader reader;
+            auto error = reader.load_file(my_obj);
+            THEN("No error is the return statement") {
+                REQUIRE(error == ObjFormatReader::NoError);
+            } AND_THEN("The number of v loaded should be the same") {
+                REQUIRE(reader.object()->v.size() == num);
+            } AND_THEN("The values of every single v should be the same") {
+                for (size_t i = 0; i < num; ++i) {
+                    REQUIRE(reader.object()->v[i] == v);
+                }
+            }
+
+        }
+
+
+
+
+    }
+
+
+
 
 }
