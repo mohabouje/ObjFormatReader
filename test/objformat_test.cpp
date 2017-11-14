c3f2eb9e045efbf44ff0ab7ed55d2aba2df3a58b
Mohammed Boujemaoui <mohabouje@gmail.com>
2017-11-14 18:18:57 +0100

    Fixing problems with the last commit, and adding more conditions in the unit test.

--- test/objformat_test.cpp
+++ test/objformat_test.cpp
@@ -2,15 +2,17 @@
 // Created by Mohammed Boujemaoui on 14/11/2017.
 //
 
-#include "../Catch2/include/catch_with_main.hpp"
+#include <fstream>
+#include "../Catch2/include/catch.hpp"
 #include "../objformat_reader.h"
 
 SCENARIO("Testing object format parser") {
     ObjFormatReader reader;
     GIVEN("A file format archive") {
-        auto my_file = "test.thing";
+        const std::string my_file = "test.thing";
+        const std::string my_obj = "test.obj";
         WHEN("The file doesnt exist") {
-            remove(my_file);
+            remove(my_file.c_str());
             AND_WHEN("We try to parse the archive") {
                 auto error = reader.load_file(my_file);
                 THEN("The error should be: EmptyFile"){
@@ -19,17 +21,26 @@ SCENARIO("Testing object format parser") {
             }
         }
 
-
-        WHEN( "The file is empty" ) {
-            remove(my_file);
-            std::ofstream outfile (my_file);
-            AND_WHEN("We try to parse the archive") {
+        WHEN( "The file is empty & not .obj format" ) {
+            remove(my_file.c_str());
+            std::ofstream outfile (my_file.c_str());
+            AND_WHEN("We try to parse the archive that is not an .obj format") {
                 auto error = reader.load_file(my_file);
-                THEN("The error should be: EmptyFile"){
+                THEN("The error should be: FormatNotSupported"){
+                    REQUIRE(error == ObjFormatReader::FormatNotSupported);
+                }
+            }
+        }
+
+        WHEN( "The file is empty and & .obj format" ) {
+            remove(my_obj.c_str());
+            std::ofstream outfile (my_obj.c_str());
+            AND_WHEN("We try to parse the archive that is an .obj format") {
+                auto error = reader.load_file(my_obj);
+                THEN("The error should be: File empty"){
                     REQUIRE(error == ObjFormatReader::FileEmpty);
                 }
             }
-
         }
     }
 
